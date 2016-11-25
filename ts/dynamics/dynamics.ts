70202dca698be5f6f808edbd41246e2caeb59f39
Matthew Jee <matthew@marmphco.com>
2016-11-24 21:48:13 -0800

    move some general convex polygon stuff from dynamics module to new polygon module

--- ts/dynamics/dynamics.ts
+++ ts/dynamics/dynamics.ts
@@ -1,17 +1,19 @@
+/// <reference path="../dynamics/actor.ts" />
 /// <reference path="../math/vector.ts" />
 /// <reference path="../math/box.ts" />
-/// <reference path="../dynamics/actor.ts" />
+/// <reference path="../math/polygon.ts" />
 
 module gerpsquirrel.dynamics {
 
     import v2 = vector2;
     
     import Box = box.Box;
+    import ConvexPolygon = polygon.ConvexPolygon;
     import Vector2 = vector2.Vector2;
 
     export class ConvexHull {
         actor: Actor;
-        private _vertices: Array<Vector2>;
+        polygon: ConvexPolygon;
 
         // For caching TODO rename these
         private _center: Vector2;
@@ -24,11 +26,12 @@ module gerpsquirrel.dynamics {
 
         constructor(vertices: Array<Vector2>) {
             this.actor = new Actor(1, 1);
+            this.polygon = new ConvexPolygon(vertices)
 
             // compute center of mass, assuming uniform mass distribution
-            const centerOfMass = convexCentroid(vertices);
+            const centerOfMass = this.polygon.centroid();
 
-            this._vertices = vertices.map((vertex) => v2.subtract(vertex, centerOfMass));
+            this.polygon.vertices = vertices.map((vertex) => v2.subtract(vertex, centerOfMass));
 
             // Set up caching properties
             this._center = this.actor.center();
@@ -41,7 +44,7 @@ module gerpsquirrel.dynamics {
         }
 
         localVertices() {
-            return this._vertices;
+            return this.polygon.vertices;
         }
 
         worldVertices() {
@@ -52,7 +55,7 @@ module gerpsquirrel.dynamics {
 
                 this._center = this.actor.center();
                 this._orientation = this.actor.orientation();
-                this._cachedWorldVertices = this._vertices.map((vertex) => this.actor.fromLocalSpace(vertex));
+                this._cachedWorldVertices = this.polygon.vertices.map((vertex) => this.actor.fromLocalSpace(vertex));
             }
                
             return this._cachedWorldVertices;
@@ -60,7 +63,7 @@ module gerpsquirrel.dynamics {
 
         worldVerticesInterpolated(t: number) {
             // cant cache these
-            return this._vertices.map((vertex) => this.actor.fromLocalSpaceInterpolated(vertex, t));
+            return this.polygon.vertices.map((vertex) => this.actor.fromLocalSpaceInterpolated(vertex, t));
         }
 
         worldBounds() {
@@ -108,50 +111,14 @@ module gerpsquirrel.dynamics {
 
             return [projectedSpan, minVertex, maxVertex];
         }
-    }
 
-    // TODO maybe this general hull stuff should be abstracted away from the dynamics stuff.
-
-    // returns the centroid of triangle with vertices `a`, `b`, and `c`.
-    //   b
-    //   |\     Medians are:
-    //   + \    u: c to mid(a, b)
-    //   |  \   v: b to mid(a, c)
-    //   a-+-c
-    export function triangleCentroid(a: Vector2, b: Vector2, c: Vector2): Vector2 {
-        
-        const base1 = v2.scale(v2.add(a, b), 0.5); // a-b midpoint
-        const base2 = v2.scale(v2.add(a, c), 0.5); 
-                      v2.by((i) => (a[i] + c[i]) / 2); // a-c midpoint
-
-        const dir1 = v2.normalize(v2.subtract(base1, c)); // a-b midpoint to c
-        const dir2 = v2.normalize(v2.subtract(base2, b)); // a-c midpoint to b
-
-        const base2Projected = v2.add(v2.project(v2.subtract(base2, base1), dir1), base1);
-        const base2ToBase2Projected = v2.subtract(base2Projected, base2);
-
-        const dir2OnDir1Length = v2.projectedLength(dir2, dir1);
-        const dir2OnDir2OrthoLength = v2.projectedLength(dir2, base2ToBase2Projected);
-
-        const distFromBase1 = v2.length(base2ToBase2Projected) * dir2OnDir1Length / dir2OnDir2OrthoLength;
-
-        return v2.add(base2Projected, v2.scale(dir1, distFromBase1));
-    }
-
-    // returns the centroid of a convex polygon
-    // vertices.length >= 3
-    export function convexCentroid(vertices: Array<Vector2>): Vector2 {
-        // Compute the average of the comprising triangle's centroids
-        var total: Vector2 = [0, 0];
-        for (var i = 2; i < vertices.length; ++i) {
-            total = v2.add(total, triangleCentroid(vertices[0], vertices[i], vertices[i - 1]));
+        contains(point: Vector2): boolean {
+            return this.polygon.contains(this.actor.toLocalSpace(point))
         }
-
-        return v2.scale(total, 1 / (vertices.length - 2));
     }
 
     export function triangleMomentOfInertia(a: Vector2, b: Vector2, c: Vector2, axis: Vector2): number {
-        const centroid = triangleCentroid(a, b, c);
+        const centroid = polygon.triangleCentroid(a, b, c);
         const base = v2.length(v2.subtract(b, c));
 
         const aOnBC = v2.add(b, v2.project(v2.subtract(a, b), v2.subtract(c, b)));
@@ -167,16 +134,6 @@ module gerpsquirrel.dynamics {
                 + height * height) / 18 + parallelAxisFactor;
     }
 
-    export function triangleArea(a: Vector2, b: Vector2, c: Vector2): number {
-        const centroid = triangleCentroid(a, b, c);
-        const base = v2.length(v2.subtract(b, c));
-
-        const aOnBC = v2.add(b, v2.project(v2.subtract(a, b), v2.subtract(c, b)));
-        const height = v2.length(v2.subtract(a, aOnBC));
-
-        return base * height / 2;
-    }
-
     // assumes that the center of mass is [0, 0]
     export function convexMomentOfInertia(hull: ConvexHull): number {
         var totalMoment: number = 0;
@@ -188,33 +145,10 @@ module gerpsquirrel.dynamics {
             const vertex1 = vertices[i];
             const vertex2 = vertices[(i + 1) % vertices.length];
             const triangleMoment = triangleMomentOfInertia(v2.ZERO, vertex1, vertex2, v2.ZERO);
-            const area = triangleArea(v2.ZERO, vertex1, vertex2);
+            const area = polygon.triangleArea(v2.ZERO, vertex1, vertex2);
             totalMoment += triangleMoment * area;
             totalArea += area;
         }
         return (totalMoment * hull.actor.mass) / totalArea;
-    }
-
-    export function hullContains(hull: ConvexHull, u: Vector2): boolean {
-        // convert point to hull space
-        const convertedPoint = hull.actor.toLocalSpace(u);
-        const vertices = hull.localVertices();
-
-        var hasClockwise = false;
-        var hasCounterClockwise = false;
-
-        for (var i = 0; i < vertices.length; i++) {
-            const base = vertices[i];
-            const tip = vertices[(i + 1) % vertices.length];
-            const edge = v2.subtract(tip, base);
-            const baseToPoint = v2.subtract(convertedPoint, base);
-            if (v2.orientation(edge, baseToPoint) == v2.Orientation.Clockwise) {
-                hasClockwise = true;
-            }
-            else {
-                hasCounterClockwise = true;
-            }
-        }
-        return hasClockwise != hasCounterClockwise;
     }
 }
