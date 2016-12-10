baf37e816b0d35c060a14864a6aae49974b015d6
Matthew Jee <matthew@marmphco.com>
2016-12-09 21:24:41 -0800

    remove Renderer

--- ts/render/render.ts
+++ ts/render/render.ts
@@ -8,27 +8,7 @@ module gerpsquirrel.render {
         renderInfo: (elapsedTime: number, updateIntervalFraction: number) => T;
     }
 
-    export interface Renderer<T> {
-        run: RenderFunction;
-        addItem: (item: Renderable<T>) => void;
-    }
-
     export interface Canvas2DRenderFunction<T> {
         (context: CanvasRenderingContext2D, item: T): void;
-    }
-
-    export function Canvas2DRendererMake<T>(context: CanvasRenderingContext2D, render: Canvas2DRenderFunction<T>): Renderer<T> {
-        var items: Array<Renderable<T>> = [];
-
-        return {
-            run: function(elapsedTime: number, updateIntervalFraction: number) {
-                items.forEach(function(item: Renderable<T>) {
-                    render(context, item.renderInfo(elapsedTime, updateIntervalFraction));
-                });
-            },
-            addItem: function(item: Renderable<T>) {
-                items.push(item);
-            }
-        };
     }
 }
