48fc1281a457424a1a75e54d421548089c521556
Matthew Jee <matthew@marmphco.com>
2017-06-10 23:25:41 -0700

    Add Lazy type

---
+++ ts/core/lazy.ts
@@ -0,0 +1,25 @@
+module gerpsquirrel.lazy {
+
+    export class Lazy<T> {
+        private _value: T
+        private _dirty: boolean
+        private _generator: () => T
+
+        constructor(generator: () => T) {
+            this._dirty = true
+            this._generator = generator
+        }
+
+        get value(): T {
+            if (this._dirty) {
+                this._value = this._generator()
+                this._dirty = false
+            }
+            return this._value
+        }
+
+        markDirty() {
+            this._dirty = true
+        }
+    }    
+}
